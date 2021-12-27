abf8b5b9531aa2b2e7935b8dbd50d5b45c47714f
Никита Кунский <oppanikita@mail.ru>
2021-12-27 12:37:29 +0300

    Добавленно 2 функции, исправлены баги

--- 29.php
+++ 29.php
@@ -2,13 +2,20 @@
 // 29. Для  каждого  числа  заданной  последовательности  натуральных   чисел n0 ,n1,...,nm
 // установить,  можно  ли  вычеркнуть  в  нем  некоторые  цифры, чтобы сумма оставшихся равнялась заданному числу к.
 
-$seekSum = 18; // Найдет много вариантов
+// Промежуток
+$x = 175000;
+$y = 180000;
+
+// Нужная сумма
+$seekSum = 40; 
+
+// Для вывода
 $counter = 0;
 $conclusion = '';
 
-//число 99 не подойдет. Хоть сумма = 18, в нем нечего вычеркнуть.
-for($i = 0; $i < 2000; $i++)
+for($i = $x; $i < $y; $i++)
 {
+    // $i = 127219;
     $number = $i;
     $count = floor(log10($number)) + 1;
 
@@ -21,52 +28,52 @@ for($i = 0; $i < 2000; $i++)
     }
 }
 
-echo "Количество найденых чисел = $counter <br><br> $conclusion";
+echo "<br>Промежуток от $x до $y; Количество найденых чисел  = $counter <br><br> $conclusion";
 
 
 
 function canCrossOutNumbers($number, $seekSum, $count) 
 {
-    //Проверка
-        // $examination == false если в $number нету цифры 0. Если в $number есть цифра 0, то $examination == true
-    $examination = false;
-    $examinationNumber = $number;
-
-    for($i = 0; $i < $count; $i++)
-    {
-        if($examinationNumber % 10 == 0)
-        {
-            $examination = true;
-            break;
-        }
-
-        $examinationNumber = intdiv($examinationNumber, 10);
-    }
+    // $zeroСheck == false если в $number нету цифры 0. Если в $number есть цифра 0, то $zeroСheck == true
+    $zeroСheck  = findDigitZero($number, $count);
 
     for($i = 0; $i < $count; $i++)
     {
         $decreasingNumber = $number;
+        $goneNumbers = 0; 
         $sum = 0; 
 
         for($k = 0; $k < $count; $k++)
         {
             $sum += $decreasingNumber % 10;
-            $decreasingNumber = intdiv($decreasingNumber, 10);
+            $goneNumbers = $goneNumbers * 10 + $decreasingNumber % 10;
 
             if($seekSum < $sum)
             {
-                break;
+                if(subtractIfSumMore($sum, $seekSum, $goneNumbers, $count))
+                {
+                    // echo ' - ';
+                    return true;
+                }
+
+                $sum -= $decreasingNumber % 10;
+
+                $decreasingNumber = intdiv($decreasingNumber, 10);
+                continue;
             }
 
             if($seekSum == $sum)
             {   
                 //проверка на, если в $number мы получили нужную сумму из цифр $number, остались ли какие-либо другие цифры в $number
-                if($k+1 < $count or $examination)
+                if(($k+1 < $count) or $zeroСheck)
                 {
+                    // echo '+';
                     return true;
                 }
                 break 2;
             }
+
+            $decreasingNumber = intdiv($decreasingNumber, 10);
         }
 
         $number = numberPermutation($number, $count);
@@ -87,4 +94,62 @@ function numberPermutation($number, $count)
     }
 
     return $number + $lastNumber;
+}
+
+//проверяет, есть ли в $number цифра 0
+function findDigitZero($number, $count)
+{
+    for($i = 0; $i < $count; $i++)
+    {
+        if($number % 10 == 0)
+        {
+            return true;
+        }
+
+        $number = intdiv($number, 10);
+    }
+
+    return false;
+}
+
+//Вычитает из $sum, цыфры из $takeSubtract, чтобы найти $seekSum
+function subtractIfSumMore($sum, $seekSum, $takeSubtract, $countNumber)
+{   
+    $countTakeSubtract = floor(log10($takeSubtract)) + 1;
+
+    // $zeroСheck == false если в $takeSubtract нету цифры 0. Если в $takeSubtract есть цифра 0, то $zeroСheck == true
+    $zeroСheck  = findDigitZero($takeSubtract, $countTakeSubtract);
+
+    for($i = 0; $i < $countTakeSubtract; $i++)
+    {
+        $decreasingNumber = $takeSubtract;
+        $currentSum = $sum;
+
+        for($k = 0; $k < $countTakeSubtract; $k++)
+        {
+            $currentSum -= $decreasingNumber % 10;
+
+            if($seekSum > $currentSum)
+            {
+                //echo "<br>$currentSum = sum $decreasingNumber = decreasingNumber<br>";
+                break;
+            }
+
+            if($seekSum == $currentSum)
+            {   
+                //проверка, остались ли какие-либо другие цифры в $takeSubtract
+                if(($countTakeSubtract < $countNumber) or ($zeroСheck) or ($k+1 < $countTakeSubtract))
+                {
+                    return true;
+                }
+                break 2;
+            }
+
+            $decreasingNumber = intdiv($decreasingNumber, 10);
+        }
+
+        $takeSubtract = numberPermutation($takeSubtract, $countTakeSubtract);
+    }
+
+    return false;
 }
